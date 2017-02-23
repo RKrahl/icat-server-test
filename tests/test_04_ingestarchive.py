73d5f4f0e02dfde529d11e566fc8fa740cd76c5a
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2017-02-23 12:09:08 +0100

    Add test_ingest_and_upload.

--- tests/test_04_ingestarchive.py
+++ tests/test_04_ingestarchive.py
@@ -2,14 +2,22 @@
 """
 
 from __future__ import print_function
+import sys
 import os
 import os.path
+import time
+from threading import Thread
+if sys.version_info < (3, 0):
+    import Queue as queue
+else:
+    import queue
 import zlib
 import logging
 import pytest
 import icat
 import icat.config
 from icat.query import Query
+from icat.ids import DataSelection
 from icat.hzb.proposal import ProposalNo
 from helper import MemorySpace, DatasetBase
 from conftest import getConfig, callscript
@@ -125,7 +133,7 @@ class Dataset(DatasetBase):
         callscript("addfile-archive.py", args)
         log.info("%s: ingest complete", self.name)
 
-    def verify(self, client):
+    def search(self, client):
         query = icat.query.Query(client, "Dataset",
                                  conditions={"name":"= '%s'" % self.name}, 
                                  includes=["datafiles"])
@@ -133,7 +141,10 @@ class Dataset(DatasetBase):
         if self.proposal.visitId:
             query.addConditions({"investigation.visitId":
                                  "='%s'" % self.proposal.visitId})
-        self.dataset = client.assertedSearch(query)[0]
+        return client.assertedSearch(query)[0]
+
+    def verify(self, client):
+        self.dataset = self.search(client)
         assert len(self.dataset.datafiles) == len(self.files), \
             "wrong number of datafiles"
         dfidx = dict([ (df.name,df) for df in self.dataset.datafiles ])
@@ -147,6 +158,83 @@ class Dataset(DatasetBase):
 # ============================= tests ==============================
 
 def test_ingest(icatconfig, client, dataset):
+    """Ingest a dataset by directly writing to archive storage.
+    """
     dataset.ingest(icatconfig)
+    dataset.verify(client)
+    dataset.download(client)
+
+def test_ingest_and_upload(icatconfig, client, dataset):
+    """Try to upload a file to the same dataset while we are ingesting it.
+    """
+
+    resultQueue = queue.Queue()
+
+    def upload(client, dataset):
+        try:
+            name = dataset.name
+            datafileFormat = dataset.getDatafileFormat(client)
+            f = Datafile(dataset.datasetdir, "upload.dat", 32)
+            while True:
+                # Get the dataset object from ICAT, continue to retry
+                # while it does not exist yet.
+                try:
+                    ds = dataset.search(client)
+                    log.info("Upload: dataset %s found.", name)
+                    break
+                except icat.SearchAssertionError:
+                    log.info("Upload: dataset %s not found (yet).", name)
+                    time.sleep(0.2)
+                    continue
+            selection = DataSelection([ds])
+            datafile = client.new("datafile", name=f.fname, 
+                                  dataset=ds, datafileFormat=datafileFormat)
+            while True:
+                # Do the upload.  This may (or even should) fail due to
+                # the dataset not online.  The error triggers a restore,
+                # so we continue to retry until the restore has been
+                # completed.
+                try:
+                    df = client.putData(f.path, datafile)
+                    log.info("Upload to dataset %s succeeded.", name)
+                    break
+                except icat.IDSDataNotOnlineError:
+                    status = client.ids.getStatus(selection)
+                    log.info("Upload: dataset %s is %s.", name, status)
+                    time.sleep(0.2)
+                    continue
+            resultQueue.put(f)
+        except Exception as err:
+            resultQueue.put(err)
+
+    def ingest(icatconfig, client, dataset):
+        try:
+            dataset.ingest(icatconfig)
+            resultQueue.put("Ok")
+        except Exception as err:
+            resultQueue.put(err)
+
+    tul = Thread(target=upload, args=(client, dataset))
+    tin = Thread(target=ingest, args=(icatconfig, client, dataset))
+    threads = [tul, tin]
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+    c = 0
+    while True:
+        try:
+            r = resultQueue.get(block=False)
+            c += 1
+            if isinstance(r, Datafile):
+                dataset.files.append(r)
+                dataset.fileCount += 1
+            elif isinstance(r, str):
+                pass
+            else:
+                raise r
+        except queue.Empty:
+            break
+    assert c == 2
     dataset.verify(client)
     dataset.download(client)
